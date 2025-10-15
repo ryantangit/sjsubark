5d5432ac6686318c0da1b1256f544943706741a4
Ryan Tan <ryantan.mail@gmail.com>
2025-10-15 13:13:26 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-echo "SJSUBarker Installation"
+echo "SJSUBarker Installation Script"
 # Setting up project folder at home directory
-mkdir $HOME/.sjsubarker/
-mkdir -p $HOME/.sjsubarker/etl/ $HOME/.sjsubarker/etl/logs/ $HOME/.sjsubarker/etl/webpages/
+
+INSTALL_DIR=$HOME
+
+if [ ! -d "$INSTALL_DIR/.sjsubarker" ]; then 
+	mkdir $INSTALL_DIR/.sjsubarker/
+fi
+
+etl_dirs=($INSTALL_DIR/.sjsubarker/etl/ $INSTALL_DIR/.sjsubarker/etl/logs/ $INSTALL_DIR/.sjsubarker/etl/webpages/)
+for dir in "${etl_dirs[@]}"; do 
+	if [ ! -d $dir ]; 
+	then
+		if mkdir $dir; then
+			echo "Created $dir"
+		fi
+	else
+		echo "$dir already exists"
+	fi
+done
 
 # Copy current config over
-cp ./etl/campus_close.json $HOME/.sjsubarker/etl/campus_close.json
+cp ./etl/campus_close.json $INSTALL_DIR/.sjsubarker/etl/campus_close.json
 
+# Create the master.csv for the records
+if [ ! -f "$INSTALL_DIR/.sjsubarker/etl/master.csv" ]; 
+then
+	if touch $INSTALL_DIR/.sjsubarker/etl/master.csv; then
+		echo "Created $INSTALL_DIR/.sjsubarker/etl/master.csv"
+	fi
+else
+	echo "$INSTALL_DIR/.sjsubarker/etl/master.csv already exists"
+fi
