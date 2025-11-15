33a72d04e945bd55fc011dc636a1a7dd938a0574
Ryan Tan <ryantan.mail@gmail.com>
2025-11-15 13:45:10 -0800

    WIP simple retrieval for records

---
+++ db/datastore/datastore.go
@@ -0,0 +1,62 @@
+package datastore
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DataStore struct {
+	dbpool *pgxpool.Pool
+}
+
+func NewDataStore(dbpool *pgxpool.Pool) *DataStore{
+	return &DataStore{dbpool: dbpool}
+}
+
+type GarageStatus struct {
+	garage_name string
+	fullness int
+	utc_timestamp time.Time
+}
+
+func (ds *DataStore) GetLatestStatus(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()	
+	query := "SELECT name, fullness, utc_timestamp FROM garage_fullness ORDER BY utc_timestamp DESC LIMIT 4;"	
+	conn, err := ds.dbpool.Acquire(r.Context())
+	if err != nil {
+		log.Println(err)
+	}
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		log.Println(err)
+	}
+	rows, err := tx.Query(ctx, query)
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	statusList := []GarageStatus{}
+	//Retrieve the utc_timestamp, name, fullness is sufficient
+	for rows.Next() {
+		var garageStatus GarageStatus
+		err := rows.Scan(&garageStatus.garage_name, &garageStatus.fullness, &garageStatus.utc_timestamp)
+		if err != nil {
+			log.Println(err)
+		}
+		statusList = append(statusList, garageStatus)
+	}
+	
+	jsonData, err := json.Marshal(statusList)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
+	
+}
+
--- db/server.go
+++ db/server.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ryantangit/sjsubark/config"
+	"github.com/ryantangit/sjsubark/db/datastore"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,12 +17,15 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello there.")
 }
 
-//TODO:
-//1. Establish ConnectionPool with Postgres
-//2. Handle for most recent garage data.
-
 func main() {
+	pool, err := pgxpool.New(context.Background(), config.PostgresURL())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+	ds := datastore.NewDataStore(pool)
 	log.Printf("Launching Server Instance")
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/latest", ds.GetLatestStatus)
 	http.ListenAndServe(":5431", nil)
 }
