924f8dde4c76d3a32d78767cb3dd38b33e8cded1
Ryan Tan <ryantan.mail@gmail.com>
2025-12-01 01:17:22 -0800

    starting code for inference server

---
+++ inference/core/trainedmodel.py
@@ -0,0 +1,18 @@
+import joblib
+from typing import Optional
+from sklearn.ensemble import HistGradientBoostingRegressor
+
+class TrainedModel:
+    def __init__(self, filepath: str):
+        self.filepath: str = filepath
+        self.model: Optional[HistGradientBoostingRegressor] = None
+
+    def load_model(self):
+        try:
+            self.model = joblib.load(self.filepath)
+            print("model loaded successfully")
+        except:
+            print("Failed to load model")
+    
+    def unload_model(self):
+        self.model = None
---
+++ inference/server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from core.trainedmodel import TrainedModel
+
+ml_model = TrainedModel("./gradient_boost_model.pkl")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ml_model.load_model()
+    yield
+    ml_model.unload_model()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", 
+            "model_loaded": ml_model.model != None}
+
+class Prediction(BaseModel):
+    name: str
+    forecast: int
+
+@app.get("/predict")
+def predict(garage: str) -> Prediction:
+    dummy = Prediction(name=garage, forecast=1)
+    return dummy
+
