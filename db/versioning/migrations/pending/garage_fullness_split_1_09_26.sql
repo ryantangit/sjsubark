12af7e02687e3dd4b816e45f9a0bce0b625174b0
Ryan Tan <ryantan.mail@gmail.com>
2026-01-10 02:35:21 -0800

    incorrect spacing fixed

--- db/versioning/migrations/pending/garage_fullness_split_1_09_26.sql
+++ db/versioning/migrations/pending/garage_fullness_split_1_09_26.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS calendar (
 	calendar_date DATE,
 	is_holiday BOOLEAN,
-	is_weekend BOOLEAN
-);
+	is_weekend BOOLEAN);
 
 CREATE TABLE IF NOT EXISTS garage_info (
 	garage_id SERIAL PRIMARY KEY,
 	garage_name VARCHAR(50) NOT NULL,
-	address VARCHAR(100) NOT NULL,
-);
+	address VARCHAR(100) NOT NULL);
