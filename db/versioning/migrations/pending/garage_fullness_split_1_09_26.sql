5c3fb8cbaaf096fe4429b754eac02b0416e0ef31
Ryan Tan <ryantan.mail@gmail.com>
2026-01-10 01:14:13 -0800

    if not exists check for table creation

--- db/versioning/migrations/pending/garage_fullness_split_1_09_26.sql
+++ db/versioning/migrations/pending/garage_fullness_split_1_09_26.sql
@@ -1,10 +1,10 @@
-CREATE TABLE calendar (
+CREATE TABLE IF NOT EXISTS calendar (
 	calendar_date date,
 	is_holiday boolean,
 	is_weekend boolean,
 );
 
-CREATE TABLE garage_info (
+CREATE TABLE IF NOT EXISTS garage_info (
 	garage_id SERIAL PRIMARY KEY,
 	garage_name VARCHAR(50) NOT NULL,
 	address VARCHAR(100) NOT NULL,
