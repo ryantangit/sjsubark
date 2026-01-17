5f8a4bda098ab45eec752266cfdff618ec1d194d
Ryan Tan <ryantan.mail@gmail.com>
2026-01-16 18:08:03 -0800

    typo

--- db/migrations/calendar_start_end_1_16_26.sql
+++ db/migrations/calendar_start_end_1_16_26.sql
@@ -1,8 +1,8 @@
-ALTER calendar
+ALTER TABLE calendar
 	ADD column IF NOT EXISTS utc_start TIMESTAMP,
 	ADD column IF NOT EXISTS utc_end TIMESTAMP;
 
-UPDATE calendar_table
+UPDATE calendar
 SET 
     utc_start = (calendar_date::text || ' 00:00:00')::timestamp AT TIME ZONE 'America/Los_Angeles' AT TIME ZONE 'UTC',
     utc_end   = (calendar_date::text || ' 23:59:59.999')::timestamp AT TIME ZONE 'America/Los_Angeles' AT TIME ZONE 'UTC';
