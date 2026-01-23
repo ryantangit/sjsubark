cd8c0c476fd7c57192f24b6f2abcfff5b6b510fb
Ryan Tan <ryantan.mail@gmail.com>
2026-01-22 16:06:44 -0800

    dockerized next js

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	experimental: {
 		optimizePackageImports: ["@chakra-ui/react"]
-	}
+	},
+	output: "standalone",
 };
 
 export default nextConfig;
