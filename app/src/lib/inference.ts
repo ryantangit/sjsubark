1dc32c6a841efb45048d5520c01b1bb9aa1f49c0
Ryan Tan <ryantan.mail@gmail.com>
2026-01-25 09:57:05 -0800

    cut back on revalidate fetch

--- app/src/lib/inference.ts
+++ app/src/lib/inference.ts
@@ -3,31 +3,36 @@ import { URL } from "url";
 import { Garage, Prediction } from "./types";
 
 export async function fetchRecentGarage(): Promise<Garage[]> {
-	try {
-		const response = await fetch(`${BACKEND_URL}/recent`, {next: {revalidate: 60}});
-		if (!response.ok) {
-			throw new Error("Fetch Latest Garage Status failed.");
-		}
-		const json = await response.json();
-		return json;
-	} catch (error) {
-		console.error(error);
-		return [];
-	}
+  try {
+    const response = await fetch(`${BACKEND_URL}/recent`, {
+      next: { revalidate: 30 },
+    });
+    if (!response.ok) {
+      throw new Error("Fetch Latest Garage Status failed.");
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
-export async function fetchGaragePrediction(garage_id: number, increments: number): Promise<Prediction | null> {
-	try {
-		const url = new URL(`${BACKEND_URL}/predict`);
-		url.searchParams.set("garage_id", garage_id.toString());
-		url.searchParams.set("increment", increments.toString());
-		const response = await fetch(`${url.toString()}`, {next: {revalidate: 60}});
-		const json = await response.json();
-		return {name: json.name, forecast: json.forecast, increments: increments};
-	} catch (error) {
-		console.error(error);
-		return null
-	}
+export async function fetchGaragePrediction(
+  garage_id: number,
+  increments: number,
+): Promise<Prediction | null> {
+  try {
+    const url = new URL(`${BACKEND_URL}/predict`);
+    url.searchParams.set("garage_id", garage_id.toString());
+    url.searchParams.set("increment", increments.toString());
+    const response = await fetch(`${url.toString()}`, {
+      next: { revalidate: 30 },
+    });
+    const json = await response.json();
+    return { name: json.name, forecast: json.forecast, increments: increments };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
-
-
