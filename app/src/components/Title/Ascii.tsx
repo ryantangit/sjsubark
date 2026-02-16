48e7f119bd465d0f4c1683814723a68f9f7c43c0
Ryan Tan <ryantan.mail@gmail.com>
2026-02-15 18:05:24 -0800

    fix color mode

--- app/src/components/Title/Ascii.tsx
+++ app/src/components/Title/Ascii.tsx
@@ -3,19 +3,21 @@ export default function AsciiArt() {
 	const art = `
 		  BARK BARK PARKING FULL?
         	\\ 
-			/ \\ / \\_       _________
-     _____  (  @  #\\___    \\\\_______\\\\
-    /     \\ /         0|    \\\\       \\\\
-   |       |/   (_____/      \\\\       \\\\
-    \\_____//     /            \\\\_______\\\\
- ____|___|/_____/______________\\\\_______\\\\____
- ______________________________________________\\
+			/ \\ / \\_      _________
+     _____  (  @  #\\___   \\\\_______\\\\
+    /     \\ /         0|   \\\\       \\\\
+   |       |/   (_____/     \\\\       \\\\
+    \\_____//     /           \\\\_______\\\\
+ ____|___|/_____/_____________\\\\_______\\\\
+ ________________________________________
 	`
 	
  	return (
-		<pre className="font-mono text-xs leading-none text-blue-500 overflow-x-auto">
+		<div className="w-full overflow-hidden">
+		<pre className="font-mono text-xs leading-none text-blue-500">
 			{art}
 		</pre>
+		</div>
 	)
 }
 
